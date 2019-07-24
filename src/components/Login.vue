d1a8787a24590faf52b53421c4a342ab2e9f3c7d
xuyu <865750866@qq.com>
2019-07-24 09:08:29 +0800

    添加一些东西

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,37 @@
 <template>
 
-  <div class="login_body" >
+  <div class="login_body">
 
-   <div class="login_input"  style="width: 450px;margin: 0 auto;position: relative;top: 25%;">
-      <el-form ref="form" :model="form" label-width="100px" >
+    <div class="login_input" style="width: 450px;margin: 0 auto;position: relative;top: 25%;">
+      <el-form ref="form" :model="form" label-width="100px">
 
         <p style="text-align: center;font-size: 24px;margin: 0px;margin-bottom: 10px;">
           <span style="position: relative;left: 30px">煜煜website</span>
         </p>
 
-        <el-form-item label="用户名:" >
+        <el-form-item label="用户名:">
           <el-col :span="24">
-          <el-input v-model="form.username" ></el-input>
+            <el-input v-model="form.username"></el-input>
           </el-col>
         </el-form-item>
 
-         <el-form-item label="密码:">
+        <el-form-item label="密码:">
           <el-col :span="24">
-        <el-input type="" v-model="form.password" show-password></el-input>
+            <el-input type="" v-model="form.password" show-password></el-input>
           </el-col>
-      </el-form-item>
+        </el-form-item>
 
         <el-form-item label="验证码:">
           <el-col :span="10">
-          <el-input v-model="form.verification_code" ></el-input>
+            <el-input v-model="form.verification_code"></el-input>
           </el-col>
+          <label style="position: relative;left: 60px;">验证码图片位置</label>
         </el-form-item>
 
         <el-form-item>
           <div style="position: relative;left: 80px;">
-          <el-button type="primary" >登陆</el-button>
-          <el-button>重置</el-button>
+            <el-button type="primary" @click="submit">登陆</el-button>
+            <el-button>重置</el-button>
           </div>
         </el-form-item>
 
@@ -52,17 +53,20 @@ export default {
         username: '',
         password: '',
         verification_code: ''
-      },
-      bgImg: require('../assets/background.jpg'),
-      contain: 'contain'
+      }
+    }
+  },
+  methods: {
+    submit () {
+      this.$router.push('/index')
     }
   }
 }
 </script>
 
 <style scoped>
-  .login_body{
+  .login_body {
     height: 100vh;
-}
+  }
 
 </style>
